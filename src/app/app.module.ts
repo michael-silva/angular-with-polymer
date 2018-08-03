8579145a493ea6121ca960597f56cb87c5ead642
michael-silva <michaelpereiradasilva@gmail.com>
2018-08-03 11:09:51 -0300

    include origami to build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PolymerModule } from '@codebakery/origami';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Origami requires the Angular Forms module
+    PolymerModule.forRoot() // Do not call .forRoot() when importing in child modules
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add to every @NgModule() that uses custom elements
   bootstrap: [AppComponent]
 })
 export class AppModule { }
