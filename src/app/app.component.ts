45af4e041e23947efe5102390703a6956c0f2e1f
michael-silva <michaelpereiradasilva@gmail.com>
2018-08-03 18:09:01 -0300

    finishing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('input') input: ElementRef;
+  tasks = [];
+
+  cleanInput() {
+    this.input.nativeElement.value = '';
+  }
+
+  addTask(title: string) {
+    this.tasks.push({ title });
+    this.cleanInput();
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  removeAll() {
+    this.tasks.splice(0);
+  }
+
+  toggleAll() {
+    const value = this.tasks.some(task => !task.checked);
+    this.tasks.forEach(task => task.checked = value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AppComponent } from './app.component';
 import { PolymerModule } from '@codebakery/origami';
 import { FormsModule } from '@angular/forms';
 
+import 'app-layout/app-layout.html';
+import 'iron-icons/iron-icons.html';
+
+import 'paper-icon-button/paper-icon-button.html';
+import 'paper-item/paper-item.html';
+import 'paper-item/paper-item-body.html';
+import 'paper-fab/paper-fab.html';
+import 'paper-dialog/paper-dialog.html';
+import 'paper-button/paper-button.html';
+import 'paper-input/paper-input.html';
+import 'paper-checkbox/paper-checkbox.html';
+
 @NgModule({
   declarations: [
     AppComponent
